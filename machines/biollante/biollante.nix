559787279eb3105e8e34019a312baac33b68bc32
Jacob Gutierrez <jacob.gutierrez28@gmail.com>
2024-06-12 14:53:19 -0500

    added recoverable session to ttyd using tmux

--- machines/biollante/biollante.nix
+++ machines/biollante/biollante.nix
@@ -31,7 +31,7 @@
   };
 
   environment.systemPackages = with pkgs; [
-
+    tmux
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
